28811a10f6e3efb1e62c55702a5567c461916f8a
Franco Miceli <francomiceli.94@gmail.com>
2021-02-01 18:28:16 -0300

    estilo al tirar resultado y primer modelo de componentes

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const app = Vue.createApp({
     return {
       teclas: ['sqrt', '^', 'sin', 'cos', 'tan', 'deg', '[', ']', '(', ')', 7, 8, 9, '*', 4, 5, 6, '-', 1, 2, 3, '+', 0, '.', '/'],
       igual: '=',
-      borrar: 'DEL',
+      borrarUno: 'DEL',
       borrarTodo: 'AC',
       display: '',
       displayStart: ''
@@ -12,10 +12,31 @@ const app = Vue.createApp({
   methods: {
     evaluar(x) {
       this.display = math.evaluate(x);
+      this.$refs.resultado.classList.add('resultado');
     },
     agregar(a) {
       this.display += a;
       this.displayStart += a;
+      this.$refs.resultado.classList.remove('resultado');
+    },
+    limpiarUno() {
+      this.display = this.display.slice(0, -1)
+    },
+    limpiarTodo() {
+      this.display = '';
     }
   }
 });
+
+/* pantalla de la calculadora según el estado:
+
+div1(start):hay dos estados de la calculadora: displayStart y display (agregar displayResult)
+se muestra displayStart al principio para que no se repita la animación de aparición
+div2: uso con 'borrarTodo'
+div3: muestra una string con la que luego se utiliza 'math(evaluate)' (pensar como almacenar el resultado
+y como generar una animación que muestre al número expandiéndose hacia arriba cuando se obtiene un resultado)
+
+// explicar el siguiente loop: (el de las teclas)
+
+*/
+
